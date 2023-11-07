b7490ef5e23e1dbc74f69f3546f3863ef4dff82b
CarlHagred <60135179+CarlHagred@users.noreply.github.com>
2023-11-07 19:15:15 +0100

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -1,19 +1,36 @@
 local M = {}
 
+M.general = {
+  n = {
+   ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left"},
+   ["<C-l>"] = { "<cmd> TmuxNavigateLeft<CR>", "window right"},
+   ["<C-j>"] = { "<cmd> TmuxNavigateLeft<CR>", "window down"},
+   ["<C-k>"] = { "<cmd> TmuxNavigateLeft<CR>", "window up"},
+  }
+}
+
 M.dap = {
   plugin = true,
   n = {
-    ["<leader>db"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line"
-    },
-    ["<leader>dus"] = {
-      function ()
-        local widgets = require('dap.ui.widgets');
-        local sidebar = widgets.sidebar(widgets.scopes);
-        sidebar.open();
-      end,
-      "Open debugging sidebar"
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+  },
+  ["<leader>dus"] = {
+    function ()
+      local widgets = require('dap.ui.widgets');
+      local sidebar = widgets.sidebar(widgets.scopes);
+      sidebar.open();
+    end,
+    "Open debugging sidebar"
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
     }
   }
 }
@@ -46,6 +63,18 @@ M.gopher = {
     ["<leader>gsy"] = {
       "<cmd> GoTagAdd yaml <CR>",
       "Add yaml struct tags"
+    }
+  }
+}
+
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
     }
   }
 }
